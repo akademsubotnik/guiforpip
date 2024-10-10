1fbe36e3ce25ebb3ad15a45086ddee32da7a7634
akademsubotnik <l1utzatjo@mozmail.com>
2024-10-09 23:02:28 -0400

    organize

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 from PyQt6 import QtWidgets, uic
-import startupwindow
+import Code.startupwindow as startupwindow
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
