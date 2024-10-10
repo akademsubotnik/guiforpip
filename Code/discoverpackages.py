c4d2674a7c0982ded9e7b3f34431b7bc23e8da8a
akademsubotnik <l1utzatjo@mozmail.com>
2024-10-10 15:44:45 -0400

    open pypi.org is working when press take me to pypi.org on discoverpackages page

--- Code/discoverpackages.py
+++ Code/discoverpackages.py
@@ -1,2 +1,13 @@
+import webbrowser
+
 def say_discoverpackages(self):
     print("Discover Packages")
+
+
+def say_takemetopypi(self):
+    print("Take me to pypi")
+    # Specify the URL you want to open
+    url = 'https://www.pypi.org'
+
+    # Open the URL in the default web browser
+    webbrowser.open(url)
--- Code/startupwindow.py
+++ Code/startupwindow.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 from PyQt6 import QtWidgets, uic
-from Code.discoverpackages import say_discoverpackages
+from Code.discoverpackages import say_discoverpackages, say_takemetopypi
 from Code.softwareupdates import say_softwareupdates
 from Code.installedpackages import say_installedpackages, print_pippackages
 
@@ -17,16 +17,20 @@ class startupwindow(QtWidgets.QDialog):
         
         #Discover Packages
         self.pushButton_discoverpackages.clicked.connect(say_discoverpackages)
-        #self.pushButton_discoverpackages.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
+        self.pushButton_discoverpackages.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
         self.pushButton_back2.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
+
+        #If take me to pypi button is pressed open pypi website
+        self.pushButton_pypi.clicked.connect(say_takemetopypi)
+        
 
         #Installed Packages
         self.pushButton_installedpackages.clicked.connect(say_installedpackages)
-        #self.pushButton_installedpackages.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
+        self.pushButton_installedpackages.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
         self.pushButton_back1.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
 
         #Software Updates
         self.pushButton_softwareupdates.clicked.connect(say_softwareupdates)
-        #self.pushButton_softwareupdates.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(3))
+        self.pushButton_softwareupdates.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(3))
         self.pushButton_back3.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))        
 
