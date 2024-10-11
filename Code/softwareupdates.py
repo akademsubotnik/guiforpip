4dfe07aa68a6915185f7e9cfe21a76a206afffb6
akademsubotnik <l1utzatjo@mozmail.com>
2024-10-10 20:28:33 -0400

    BIG update, understanding how to add functions to classes!!!

--- Code/softwareupdates.py
+++ Code/softwareupdates.py
@@ -5,6 +5,9 @@ class SoftwareUpdates:
 
     def setup_ui(self):
         # Software Updates
-        #self.parent.pushButton_softwareupdates.clicked.connect(swu.say_softwareupdates)
+        self.parent.pushButton_softwareupdates.clicked.connect(self.say_software_updates)
         self.parent.pushButton_softwareupdates.clicked.connect(lambda: self.parent.stackedWidget.setCurrentIndex(3))
         self.parent.pushButton_back3.clicked.connect(lambda: self.parent.stackedWidget.setCurrentIndex(0))
+
+    def say_software_updates(self):
+        print("Software Updates")
